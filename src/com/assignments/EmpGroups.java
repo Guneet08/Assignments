be6177cb7c4c238c9bbb20f19d4748c3c68f1bd6
Guneet <gntchhbr@gmail.com>
2025-02-03 06:12:29 +0530

    Assignment 3 |classes

---
+++ src/com/assignments/EmpGroups.java
@@ -0,0 +1,16 @@
+package com.assignments;
+
+
+public class EmpGroups {
+	public static void main(String[] args) {
+
+	Employee obj = new Employee();
+		String[] empGroupNames = obj.empNames;
+		int[] empGroupId = obj.empId;
+		
+		System.out.println(empGroupNames[0]);
+		System.out.println(empGroupId[0]);
+
+	}
+
+}
---
+++ src/com/assignments/Employee.java
@@ -0,0 +1,19 @@
+package com.assignments;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		String[] empNames = new String[3];
+		empNames[0] = "Bharath";
+		empNames[1] = "ABC";
+		empNames[2] = "XYZ";
+		
+		int[] empId = new int[3];
+		empId[0] = 	1234;
+		empId[1] = 1235;
+		empId[2] = 1236;
+		
+
+	}
+
+}
